50c298264d774292ed9603c718472df7aaf83ddd
sebastiantare <sebastiantarebustos@gmail.com>
2024-06-21 19:55:28 -0400

    api routes & resources

--- backend/api-rest/routes/api.php
+++ backend/api-rest/routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
+use App\Http\Controllers\CalleController;
+use App\Http\Controllers\CiudadController;
+use App\Http\Controllers\ProvinciaController;
+use App\Http\Controllers\RegionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::apiResource('regiones', RegionController::class);
+//Route::apiResource('provincias', ProvinciaController::class);
+//Route::apiResource('ciudades', CiudadController::class);
+//Route::apiResource('calles', CalleController::class);
+
+Route::resource('regiones', RegionController::class)->parameters(['regiones' => 'region']);
+Route::resource('ciudades', CiudadController::class)->parameters(['ciudades' => 'ciudad']);
+Route::resource('provincias', ProvinciaController::class)->parameters(['provincias' => 'provincia']);
+Route::resource('calles', CalleController::class)->parameters(['calles' => 'calle']);
